1b637ee06485c3ab82272046341f7d7cfa662220
nick90903 <nhatchew03@gmail.com>
2022-04-16 07:19:23 -0500

    Adds Player input forms
    Adds seperate forms for Player One and Player Two to input
    their name and desired symbol

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let gameboard_Values = [];
 let gameboard_Elements = [];
 let gameboard_Background = document.querySelector('.board-Background');
 
+let player_One_Configurator = document.querySelector('.input-One');
+player_One_Configurator.style.display = 'none';
+
+let player_Two_Configurator = document.querySelector('.input-Two');
+player_Two_Configurator.style.display = 'none';
+
 for(let i = 0; i < 9; i++) {
     let temp = document.createElement('div');
     temp.value = i;
@@ -26,4 +32,20 @@ const nextTurn = (playerID, playAreaValue) => {
 
 function showBoard(element) {
     let inputOne = document.querySelector('');
+}
+
+function showForm(element) {
+    console.log('show form clicked' + element.value);
+    let test = element.value;
+    switch(test) {
+        case 0:
+            console.log('0');
+            player_One_Configurator.style.display = 'flex';
+            break;
+        
+        case 1:
+            console.log('2');
+            player_Two_Configurator.style.display = 'flex';4
+            break;
+    }
 }
