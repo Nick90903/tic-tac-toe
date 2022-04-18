71fd13dc8addc17b9f5204a2044b50a22844cf45
nick90903 <nhatchew03@gmail.com>
2022-04-18 08:43:53 -0500

    Fixes Victory Check

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let gameboard_Elements = [];
 let gameboard_Background = document.querySelector('.board-Background');
 let current_Player = true;
 
+
 for(let i = 0; i < 9; i++) {
     let temp = document.createElement('div');
     temp.value = i;
     temp.classList.add('play-Area');
+    temp.textContent = ('');
     temp.addEventListener('click', function() {
         playTurn.turnPlayed(this);
     })
@@ -15,14 +17,14 @@ for(let i = 0; i < 9; i++) {
     console.table(gameboard_Elements);
 }
 
-
-const player = (element, ) => {
+const player = (element) => {
     let score;
-    let playerID = element.value;
+    let playerID = element;
     const getScore = () => score;
     const setScore = () => score++;
-}
 
+    return {setScore, getScore};
+}
 
 function swapPlayer () {
 
@@ -31,56 +33,90 @@ function swapPlayer () {
     current_Player = !current_Player;
 
     if(current_Player) {
-        current_Player_text.textContent = ('X: <--');
+        current_Player_text.textContent = ('<-- X');
     } else {
-        current_Player_text.textContent = ('--> :Y');
+        current_Player_text.textContent = ('O -->');
     }
     playTurn.updateCurrentPlayer(current_Player);
 }
 
+let turnsPlayed = 0;
+
 const playTurn = (() => {
     let activePlayer;
-
+    
     const updateCurrentPlayer = (current) => {
         activePlayer = current;
-        console.log('Update Current Player' + activePlayer);
     }
 
     const turnPlayed = (element) => {
         console.log(`${element.value} Clicked`);
-        activePlayer ? element.textContent = ('X') : element.textContent = ('O');
-        swapPlayer();
-        checkVictory();
+        if(element.textContent == '') {
+            activePlayer ? element.textContent = ('X') : element.textContent = ('O');
+            turnsPlayed = turnsPlayed + 1;
+            swapPlayer();
+            checkVictory();
+        }
     }
 
     const checkVictory = () => {
-        // Check col
-        for(let i = 0; i <= 2; i++) {
-            if(gameboard_Elements[i].textContent == gameboard_Elements[3 + i].textContent && gameboard_Elements[i + 6].textContent) {
-                console.log(`${gameboard_Elements[i].textContent} Wins Col`);
+        if(turnsPlayed >= 0) {
+            
+            // Check col
+            for(let i = 0; i <= 2; i++) {
+                if(gameboard_Elements[i].textContent == gameboard_Elements[3 + i].textContent && gameboard_Elements[i + 6].textContent == gameboard_Elements[i].textContent) {
+                    if(gameboard_Elements[i].textContent != '') {
+                        console.log(`${gameboard_Elements[i].textContent} Wins Col in spot ${i}`);
+                        return displayWinner(gameboard_Elements[i].textContent);
+                    }
+                }
+            }
+
+            // Check row
+            for(let i = 0; i <= 2; i++) {
+                if(gameboard_Elements[0 + (i * 3)].textContent == gameboard_Elements[1 + (i * 3)].textContent && gameboard_Elements[2 + (i * 3)].textContent == gameboard_Elements[0 + (i * 3)].textContent) {
+                    if(gameboard_Elements[0 + (i * 3)].textContent != '') {
+                        console.log(`${gameboard_Elements[i * 3].textContent} Wins Row`);
+                        return displayWinner(gameboard_Elements[0 + (i * 3)].textContent);
+                    }
+                }
+            }
+
+            //check \
+            if(gameboard_Elements[0].textContent == gameboard_Elements[4].textContent && gameboard_Elements[8].textContent == gameboard_Elements[0].textContent) {
+                if(gameboard_Elements[0].textContent != '') {
+                    console.log(`${gameboard_Elements[0].textContent} Wins `);
+                    return displayWinner(gameboard_Elements[0].textContent);
+                }
+            }
+
+            //check /
+            if(gameboard_Elements[2].textContent == gameboard_Elements[4].textContent && gameboard_Elements[6].textContent == gameboard_Elements[2].textContent) {
+                if(gameboard_Elements[2].textContent != '') {
+                    console.log(`${gameboard_Elements[2].textContent} Wins /`);
+                    return displayWinner(gameboard_Elements[2].textContent);
+                }
             }
         }
+    }
 
-        // Check row
-        for(let i = 0; i <= 2; i++) {
-            console.log(3 + (i * 3));
-            if(gameboard_Elements[0 + (i * 3)].textContent == gameboard_Elements[1 + (i * 3)].textContent && gameboard_Elements[2 + (i * 3)].textContent) {
-                console.log(`${gameboard_Elements[i * 3].textContent} Wins Row`);
-            }
-        }
 
-        //check \
-        if(gameboard_Elements[0].textContent == gameboard_Elements[4].textContent && gameboard_Elements[8].textContent) {
-            console.log(`${gameboard_Elements[0].textContent} Wins `);
-        }
+    const displayWinner = (winner) => {
+        let current_Player_text = document.querySelector('.next');
+        let player1_Score = document.querySelector('.primary-Score');
+        let player2_Score = document.querySelector('.second-Score');
 
-        //check /
-        if(gameboard_Elements[2].textContent == gameboard_Elements[4].textContent && gameboard_Elements[6].textContent) {
-            console.log(`${gameboard_Elements[2].textContent} Wins /`);
+        if(winner == 'X') {
+            Player1.setScore();
+            current_Player_text.textContent = ('X Wins!')
+            player1_Score.textContent = (player1.getscore());
         }
     }
 
     return {updateCurrentPlayer, turnPlayed};
 })();
+
+let player1 = player(1);
+let player2 = player(2);
 
 swapPlayer();
