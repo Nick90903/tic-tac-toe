5d0e731a20070d7923cf93ff92be2f914dbf5531
nick90903 <nhatchew03@gmail.com>
2022-04-18 04:11:44 -0500

    Removes legacy player Form code

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ let gameboard_Elements = [];
 let gameboard_Background = document.querySelector('.board-Background');
 let current_Player = true;
 
-/*
-let player_One_Configurator = document.querySelector('.input-One');
-player_One_Configurator.style.display = 'none';
-
-let player_Two_Configurator = document.querySelector('.input-Two');
-player_Two_Configurator.style.display = 'none';
-*/
-
-
 for(let i = 0; i < 9; i++) {
     let temp = document.createElement('div');
     temp.value = i;
@@ -92,23 +83,4 @@ const playTurn = (() => {
     return {updateCurrentPlayer, turnPlayed};
 })();
 
-function showBoard(element) {
-    let inputOne = document.querySelector('');
-}
-
 swapPlayer();
-
-
-/*
-function showForm(element, property) {
-    console.log('show form clicked' + element.value);
-    let test = element.value;
-    if(element.value == 0) {
-        player_One_Configurator.style.display = (property);
-        player_Two_Configurator.style.display = ('none');
-    } else {
-        player_Two_Configurator.style.display = (property);
-        player_One_Configurator.style.display = ('none');
-    }
-}
-*/
